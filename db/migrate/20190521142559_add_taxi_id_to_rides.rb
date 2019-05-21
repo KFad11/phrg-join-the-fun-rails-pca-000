754dc955df737ba33e9f4f85c38488f4530cab87
KFad <kfadigan@yahoo.com>
2019-05-21 10:35:34 -0400

    create activerecord associations

---
+++ db/migrate/20190521142559_add_taxi_id_to_rides.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class AddTaxiIdToRides < ActiveRecord::Migration
+  def change
+    change_table :rides do |t|
+      t.integer :taxi_id
+    end
+  end
+end
---
+++ db/migrate/20190521143358_add_passenger_id_to_rides.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class AddPassengerIdToRides < ActiveRecord::Migration
+  def change
+    change_table :rides do |t|
+      t.integer :passenger_id
+    end
+  end
+end
